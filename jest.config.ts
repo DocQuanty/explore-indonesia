44f33e5cabb6ad188c25f9fcbf6731232f5a7bea
Yurchenko K <Urchikak8@gmail.com>
2023-08-11 12:42:37 +0300

    Trying setting jest and working at it

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,14 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
   verbose: true,
   preset: 'ts-jest',
 };
