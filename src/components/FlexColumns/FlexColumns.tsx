345b6e412f5261ba849394e88fb1ec73abdd426a
Костянтин <71712209+DocQuanty@users.noreply.github.com>
2023-08-25 17:48:58 +0300

    flex style fix, added map component

--- src/components/FlexColumns/FlexColumns.tsx
+++ src/components/FlexColumns/FlexColumns.tsx
@@ -1,22 +1,27 @@
 import s from './FlexColumns.module.scss';
 
 const FlexColumns = () => {
-return <div className={s.flexColumns}>
-    <div className={s.flexColumns_card}>
-        <div className={s.card_number}>01</div>
-        <div className={s.card_title}>Title here</div>
-        <div className={s.card_description}>Lorem ipsum dolor sit amet et amet ist ame.</div>
-    </div>
-    <div className={s.flexColumns_card}>
-        <div className={s.card_number}>01</div>
-        <div className={s.card_title}>Title here</div>
-        <div className={s.card_description}>Lorem ipsum dolor sit amet et amet ist ame.</div>
-    </div>
-    <div className={s.flexColumns_card}>
-        <div className={s.card_number}>01</div>
-        <div className={s.card_title}>Title here</div>
-        <div className={s.card_description}>Lorem ipsum dolor sit amet et amet ist ame.</div>
-    </div>
-</div>
+    const arr = [
+        { number: "01", title: "title", description: "description" },
+        { number: "02", title: "title", description: "description" },
+        { number: "03", title: "title", description: "description" }
+    ]
+
+    return (
+        <div className={s.flexColumns}>
+            {
+                arr.map((e) => (
+                    <div key={e.number} className={s.flexColumns_card}>
+                        <div className={s.card_number}>{e.number}</div>
+                        <div className={s.card_title}>Title here</div>
+                        <div className={s.card_description}>Lorem ipsum dolor sit amet et amet ist ame.</div>
+                    </div>
+                ))
+
+            }
+        </div>
+    )
+
 };
+
 export default FlexColumns;
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,29 +1,27 @@
 import s from "../Landing/landing.module.scss"
 import Slider from "../../components/Slider/Slider";
-import DefaultText from "../../components/DefaultText/DefaultText"; 
+import DefaultText from "../../components/DefaultText/DefaultText";
 import ButtonMain from "../../components/Button/ButtonMain";
 import FlexColumns from "../../components/FlexColumns/FlexColumns"
 
 const Landing = () => {
-return(
-    <div className={s.landing} >
-        <Slider />
-        <div className={s.landing_title}>
-        <h1 className={s.title_text} >
-        <DefaultText  
-            textTransform={"uppercase"} 
-            maxWidth={"627px"} 
-            text={"explore indonesia"}
-            fontSize={"120px"}
-            fontFamily={"Roboto"}
-            />
-        </h1>
-        <ButtonMain padding={"20px 50px"}/>
+    return (
+        <div className={s.landing} >
+            <Slider />
+            <div className={s.landing_title}>
+                <h1 className={s.title_text} >
+                    <DefaultText
+                        textTransform={"uppercase"}
+                        maxWidth={"627px"}
+                        text={"explore indonesia"}
+                        fontSize={"120px"}
+                        fontFamily={"Roboto"}
+                    />
+                </h1>
+                <ButtonMain padding={"20px 50px"} />
+            </div>
+            <FlexColumns />
         </div>
-        <div className="text-flex">
-        <FlexColumns/>
-        </div>
-    </div>
     )
 };
 export default Landing;
