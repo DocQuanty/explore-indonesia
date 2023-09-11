0b7d3fb75e5118ef5b709f03afc58239beb6446a
Костянтин <71712209+DocQuanty@users.noreply.github.com>
2023-09-11 18:45:06 +0300

    added media require from container and block navigation

--- src/blocks/BlockNav/BlockNav.tsx
+++ src/blocks/BlockNav/BlockNav.tsx
@@ -6,25 +6,23 @@ import FlexColumns from "../../components/FlexColumns/FlexColumns"
 
 const BlockNav = () => {
     return (
-        <div className={s.landing} >
-            <div className={s.landing_container}>
-                <Slider />
-                <div className={s.landing_title}>
-                    <h1 className={s.title_text} >
-                        <DefaultText
-                            textTransform={"uppercase"}
-                            text={"explore indonesia"}
-                            fontFamily={"Roboto"}
-                        />
-                    </h1>
-                    <ButtonMain className={"buttonMain"} href={"/start_travelling"} text={"Start travelling"} />
-                </div>
-                <FlexColumns />
-                <div className={s.additionalLinks}>
-                    <a className={s.additionalLinks_span} href="/Fb">Fb</a>
-                    <a className={s.additionalLinks_span} href="/Tw">Tw</a>
-                    <a className={s.additionalLinks_span} href="/In">In</a>
-                </div>
+        <div className={s.landing_container}>
+            <Slider />
+            <div className={s.landing_title}>
+                <h1 className={s.title_text} >
+                    <DefaultText
+                        textTransform={"uppercase"}
+                        text={"explore indonesia"}
+                        fontFamily={"Roboto"}
+                    />
+                </h1>
+                <ButtonMain className={"buttonMain"} href={"/start_travelling"} text={"Start travelling"} />
+            </div>
+            <FlexColumns />
+            <div className={s.additionalLinks}>
+                <a className={s.additionalLinks_span} href="/Fb">Fb</a>
+                <a className={s.additionalLinks_span} href="/Tw">Tw</a>
+                <a className={s.additionalLinks_span} href="/In">In</a>
             </div>
         </div>
     )
