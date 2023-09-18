7bd16663a84a8e351fc0dd8e59ff7e82fecd0644
Yurchenko K <Urchikak8@gmail.com>
2023-09-18 15:48:32 +0300

    rewrite blockDefault text added mediascrin

--- src/blocks/ImageTextBlock/ImageTextBlock.tsx
+++ src/blocks/ImageTextBlock/ImageTextBlock.tsx
@@ -7,7 +7,7 @@ const ImageTextBlock = () => {
             <div className={s.image}></div>
             <div className={s.text}>
                 <div className={s.text_title}>
-                    <DefaultText text='The walkways of Indonesia spellbind' maxWidth="10px" color='black' />
+                    <DefaultText text='The walkways of Indonesia spellbind' className='title' />
                 </div>
                 <div className={s.text_image}>Lorem ipsum dolor sit amet, consectetur adip iscing elit. Fusce commodo magna et libero mollis rhoncus. </div>
                 <a className={s.text_btn}>Learn More</a>
