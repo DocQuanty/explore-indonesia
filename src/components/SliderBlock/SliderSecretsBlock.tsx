df4c1596ff0682f830b607a5846f3ae6daead85d
Костянтин <71712209+DocQuanty@users.noreply.github.com>
2023-11-19 14:01:37 +0200

    SliderSecretsBlock created media query

--- src/components/SliderBlock/SliderSecretsBlock.tsx
+++ src/components/SliderBlock/SliderSecretsBlock.tsx
@@ -9,7 +9,9 @@ const SliderSecretsBlock = () => {
         <div className={s.sliderSecret}>
             <SliderSecret />
         </div>
+        {/* darkBlueSection */}
         <div className={s.SliderBlockNumber}>
+            {/* Title-section */}
             <section className={s.NumberBlock}>
                 <div className={s.NumberBlock_mainTitle}>
                     By The Numbers
@@ -17,13 +19,24 @@ const SliderSecretsBlock = () => {
                 <div className={s.NumberBlock_mainDescription}>
                     <DefaultText text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo magna et libero.Lorem ipsum dolor sit amet, consectetur adipiscing elit." className='additional_GreyText_center' />
                 </div>
-                <div>
-                    <div className={s.NumberBlockCard}>
-                        <div className={s.NumberBlockCard_wrapper}>
-                            <div className={s.NumberBlockCard_title}>100+</div>
-                            <div className={s.NumberBlockCard_description}>
-                                <DefaultText text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo magna et libero." className='additional_GreyText_center' />
-                            </div>
+                {/* cards-item*/}
+                <div className={s.NumberBlockCard}>
+                    <div className={s.NumberBlockCard_flexCards}>
+                        <div className={s.NumberBlockCard_title}>100+</div>
+                        <div className={s.NumberBlockCard_description}>
+                            <DefaultText text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo magna et libero." className='additional_GreyText_center' />
+                        </div>
+                    </div>
+                    <div className={s.NumberBlockCard_flexCards}>
+                        <div className={s.NumberBlockCard_title}>43,000+</div>
+                        <div className={s.NumberBlockCard_description}>
+                            <DefaultText text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo magna et libero." className='additional_GreyText_center' />
+                        </div>
+                    </div>
+                    <div className={s.NumberBlockCard_flexCards}>
+                        <div className={s.NumberBlockCard_title}>30+</div>
+                        <div className={s.NumberBlockCard_description}>
+                            <DefaultText text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo magna et libero." className='additional_GreyText_center' />
                         </div>
                     </div>
                 </div>
